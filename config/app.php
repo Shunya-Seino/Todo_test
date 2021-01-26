6f5baf6585217ec051d2e947989d32e2814ead2e
Shunya Seino <seino@feelist.co.jp>
2021-01-26 02:35:55 +0000

    リダイレクトテスト

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://seino-kintai-todo.herokuapp.com/login/'),
+    'url' => env('APP_URL', 'https://seino-kintai-todo.herokuapp.com/'),
 
     'asset_url' => env('ASSET_URL', null),
 
