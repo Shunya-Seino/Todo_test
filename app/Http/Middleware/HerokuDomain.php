fe7f142eeb8970f84e418152239fe36e1a11dec5
Shunya Seino <seino@feelist.co.jp>
2021-01-26 01:14:31 +0000

    リダイレクトテスト

--- app/Http/Middleware/HerokuDomain.php
+++ app/Http/Middleware/HerokuDomain.php
@@ -8,8 +8,6 @@ class HerokuDomain {
   public function handle($request, Closure $next)
   {
     if (substr($request->header('host'), -13) === 'herokuapp.com') {
-        dump(substr($request->header('host'), -13));
-        dd(redirect()->away(config('app.url').$_SERVER['REQUEST_URI']));
         // ドメイン末尾がherokuapp.comのアクセスの場合、.envで指定したAPP_URLに遷移させる
         return redirect()->away(config('app.url') . $_SERVER['REQUEST_URI']);
     }
